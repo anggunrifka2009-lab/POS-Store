58f7230c9b6121166ee16f9fb075e6258372d640
Anggun <anggunrifka@gmail.com>
2026-05-30 18:20:48 +0700

    commit

---
+++ app/src/main/java/com/anggun/pos/model/ModelPegawai.kt
@@ -0,0 +1,13 @@
+package com.anggun.pos.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ModelPegawai(
+    var idPegawai: String? = null,
+    var nama: String? = null,
+    var alamat: String? = null,
+    var role: String? = "Kasir",
+    var foto: String? = null
+) : Parcelable
